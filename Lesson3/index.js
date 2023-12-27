71efe0df73e6bbaf93c4c49cb3ee6d4e2489fb37
Sergey <rodenkosv@gmail.com>
2023-12-27 23:29:26 +0700

    adding javascript code

--- Lesson3/index.js
+++ Lesson3/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 
-// Подключение библиотечки для валидации
-const Joi = require('joi');
+const fs = require('fs').promises; 
+
+const Joi = require('joi'); // Подключение библиотечки для валидации
 
 const userSchema = Joi.object({
     name: Joi.string().min(3).required(),
@@ -14,76 +15,123 @@ const app = express();
 
 app.use(express.json());
 
-const users = [];
-let uId = 0;
+let users = [];
+
+// Функция для чтения пользователей из файла
+function readUsersFromFile() {
+    return fs.readFile('users.json', 'utf8')
+        .then((data) => {
+            users = JSON.parse(data);
+        })
+        .catch((err) => {
+            console.error('Ошибка чтения файла:', err);
+            users = [];
+        });
+}
+
+// Функция для записи пользователей в файл
+function writeUsersToFile() {
+    return fs.writeFile('users.json', JSON.stringify(users, null, 2), 'utf8')
+        .catch((err) => {
+            console.error('Ошибка записи в файл:', err);
+        });
+}
 
 app.get('/users', (req, res) => {
-    res.send({ users });
+    readUsersFromFile()
+        .then(() => {
+            res.send({ users });
+        });
 });
 
 app.get('/users/:id', (req, res) => {
     const user = users.find((user) => user.id === +(req.params.id));
-    if(user) {
-        res.send({ user });
-    } else {
-        res.status(404);
-        res.send({ user: null });
-    }    
+    readUsersFromFile()
+        .then(() => {            
+            if (user) {
+                res.send({ user });
+            } else {
+                res.status(404).send({ user: null });
+            }
+        });
 });
 
 app.post('/users', (req, res) => {
-    const result = userSchema.validate(req.body); 
+    const result = userSchema.validate(req.body);
 
     if (result.error) {
-        return res.status(500).send({ error: result.error.details })
+        return res.status(500).send({ error: result.error.details });
+    }
 
-    };
-
-    uId += 1;
-    users.push({
-        id: uId,
-        ...req.body,
-    });
-    res.send({ id: uId });
+    const userId = users.length > 0 ? users[users.length - 1].id + 1 : 1;
+    readUsersFromFile()
+        .then(() => {           
+            users.push({
+                id: userId,
+                ...req.body,
+            });
+            return writeUsersToFile();  // Возвращаем промис для обработки успешной записи
+        })
+        .then(() => {
+            res.send({ id: userId });  // Отправляем ответ только после успешной записи
+        })
+        .catch((err) => {
+            console.error('Ошибка при добавлении пользователя:', err);
+            res.status(500).send('Ошибка сервера');
+        });
 });
 
 app.put('/users/:id', (req, res) => {
-    const result = userSchema.validate(req.body); 
+    const result = userSchema.validate(req.body);
 
     if (result.error) {
-        return res.status(500).send({ error: result.error.details })
-
-    };
-
-    const user = users.find((user) => user.id === +(req.params.id));
-    
-    if(user) {
-        user.name = req.body.name;
-        user.lastName = req.body.lastName;
-        user.age = req.body.age;
-        user.city = req.body.city;
-
-        res.send({ user });
-    } else {
-        res.status(404);
-        res.send({ user: null });
+        return res.status(500).send({ error: result.error.details });
     }
+
+    const userId = parseInt(req.params.id);
+    const index = users.findIndex(user => user.id === userId);
+
+    readUsersFromFile()
+        .then(() => {
+            if (index !== -1) {
+                users[index] = { id: userId, ...req.body };                
+                return writeUsersToFile().then(() => {
+                    res.send({ user: users[index] });
+                });   
+            } else {
+                res.status(404).send({ user: null });
+            }
+        })             
+        .catch((err) => {
+            console.error('Ошибка при обновлении пользователя:', err);
+            res.status(500).send('Ошибка сервера');
+        });
 });
 
 app.delete('/users/:id', (req, res) => {
-    const user = users.find((user) => user.id === +(req.params.id));
-    
-    if(user) {
-        const userIndex = users.indexOf(user);
+    const userId = parseInt(req.params.id);
 
-        users.splice(userIndex, 1);
-
-        res.send({ user });
-    } else {
-        res.status(404);
-        res.send({ user: null });
-    }
+    readUsersFromFile()
+        .then(() => {
+            const index = users.findIndex(user => user.id === userId);
+            if (index !== -1) {
+                users.splice(index, 1);
+                return writeUsersToFile().then(() => {
+                    res.send('Пользователь успешно удален');          
+                }); 
+            } else {
+                res.status(404).send('Пользователь не найден');
+            }
+        })
+        .catch((err) => {
+            console.error('Ошибка при удалении пользователя:', err);
+            res.status(500).send('Ошибка сервера');
+        });
 });
 
-
-app.listen(3000);
+app.listen(3000, () => {
+    readUsersFromFile()
+        .then(() => {
+            console.log('Сервер запущен на порте 3000');
+        });
+});
